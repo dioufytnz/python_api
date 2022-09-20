fe7d91444f860960157f0e574c310a557609a320
mdiouf2 <mamadou.diouf2@orange-sonatel.com>
2022-09-20 16:05:20 +0000

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,10 +1,13 @@
-import multiprocessing
+from multiprocessing import cpu_count
 
-workers = multiprocessing.cpu_count() * 2 + 1
-bind = 'unix:flaskrest.sock'
-umask = 0o007
-reload = True
+# Socket Path
+bind = 'unix:/projets/python_api/gunicorn.sock'
 
-#logging
-accesslog = '-'
-errorlog = '-'
+# Worker Options
+workers = cpu_count() + 1
+worker_class = 'uvicorn.workers.UvicornWorker'
+
+# Logging Options
+loglevel = 'debug'
+accesslog = '/var/log/apache2/access_FastApi_log'
+errorlog = '/var/log/apache2/error_FastApi_log'
