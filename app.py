282ff1ae3d36d40b3d3be7fb411258ce699e413c
mdiouf2 <mamadou.diouf2@orange-sonatel.com>
2022-09-21 18:37:53 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/{index}/{user}/{device}/{kpi}/{start_time}/{end_time}")
+@app.get("/iot/{index}/{user}/{device}/{kpi}/{start_time}/{end_time}")
 async def root(index, user, device, kpi, start_time, end_time):
     return {"message": index + "*" + user + "*" + device + "*" + kpi + "*" + start_time + "*" + end_time}
